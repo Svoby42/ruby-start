3d68fc58736bfb204baa4a6f4be778a1d0cffc1c
svoby42 <qwerta009@gmail.com>
2022-03-07 23:10:53 +0100

    loops

--- Loop.rb
+++ Loop.rb
@@ -19,3 +19,22 @@ while y <= 10
 end
 
 puts "haha" unless (y = 1)
+
+z = 1
+
+until z >= 10
+  z += 1
+  next unless (z % 2) == 0
+  puts z
+end
+
+numbers = [1,2,3,4,5]
+for number in numbers
+  puts "#{number}, "
+end
+
+groceries = ["bananas", "potatoes", "daniela"]
+
+groceries.each do |food|
+  puts "Get #{food}"
+end
