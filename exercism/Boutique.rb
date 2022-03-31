ff7979967ead37df699a6bdb6938b2e8442c07d0
Ondrej Svoboda <admin@svoby.eu>
2022-03-31 19:45:50 +0200

    boutique

---
+++ exercism/Boutique.rb
@@ -0,0 +1,45 @@
+class BoutiqueInventory
+  def initialize(items)
+    @items = items
+  end
+
+  def item_names
+    @items.map {|item| item[:name]}.sort
+  end
+
+  def cheap
+    @items.select { |item| item[:price] < 30.00 }
+  end
+
+  def out_of_stock
+    @items.select { |item| item[:quantity_by_size].empty? }
+
+  end
+
+  def stock_for_item(name)
+    @items.find { |item| item[:name] == name }[:quantity_by_size]
+  end
+
+  def total_stock
+    sum = 0
+    @items.each { |item| item[:quantity_by_size].each { |key, value| sum += value } }
+    sum
+  end
+
+  private
+  attr_reader :items
+end
+
+inventory =  BoutiqueInventory.new([
+                          {price: 65.00, name: "Maxi Brown Dress", quantity_by_size: {s: 3, m: 7, l: 8, xl: 4}},
+                          {price: 50.00, name: "Red Short Skirt", quantity_by_size: {}},
+                          {price: 29.99, name: "Black Short Skirt", quantity_by_size: {s: 1, xl: 4}},
+                          {price: 20.00, name: "Bamboo Socks Cats", quantity_by_size: {s: 7, m: 2}}
+                        ])
+
+p inventory.item_names
+p inventory.cheap
+p inventory.out_of_stock
+p inventory.stock_for_item("Black Short Skirt")
+puts "NIGGA"
+puts inventory.total_stock
